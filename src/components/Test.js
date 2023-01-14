12aeb4ef07265cab2f9e262d49d8f43b8ba5a63e
Castro-1 <jecastrog@eafit.edu.co>
2023-01-14 17:10:03 -0500

    forms theory done

--- src/components/Test.js
+++ src/components/Test.js
@@ -476,25 +476,245 @@ import React from "react";
 //   );
 // }
 
-export default function Test() {
-  const [messages, setMessages] = React.useState(["a", "b"]);
+// export default function Test() {
+//   const [messages, setMessages] = React.useState(["a", "b"]);
+//   /**
+//    * Challenge:
+//    * - If there are no unread messages, display "You're all caught up!"
+//    * - If there are > 0 unread messages, display "You have <n> unread
+//    *   message(s)"
+//    *      - If there's exactly 1 unread message, it should read "message"
+//    *        (singular)
+//    */
+//   const displayFrase =
+//     messages.length > 0
+//       ? `You have ${messages.length} unread message${
+//           messages.length > 1 ? "s" : ""
+//         }`
+//       : "You're all caught up!";
+//   return (
+//     <div>
+//       <h1>{displayFrase}</h1>
+//     </div>
+//   );
+// }
+
+// export default function Form() {
+//   const [formData, setFormData] = React.useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     comments: "",
+//     isFriendly: true,
+//     employment: "",
+//     favColor: "",
+//   });
+
+//   function handleChange(event) {
+//     /**
+//      * Challenge: update the firstName state on every keystroke
+//      */
+//     const { name, value, type, checked } = event.target;
+//     setFormData((prevFormData) => {
+//       return {
+//         ...prevFormData,
+//         [name]: type === "checkbox" ? checked : value,
+//       };
+//     });
+//   }
+//   /**
+//    * Challenge: Track the applicant's last name as well
+//    */
+//   /**
+//    * Challenge: add an email field/state to the form
+//    */
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     // submitToApi(formData)
+//     console.log(formData);
+//   }
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         placeholder="First Name"
+//         onChange={handleChange}
+//         name="firstName"
+//         value={formData.firstName}
+//       />
+//       <input
+//         type="text"
+//         placeholder="Last Name"
+//         onChange={handleChange}
+//         name="lastName"
+//         value={formData.lastName}
+//       />
+//       <input
+//         type="email"
+//         placeholder="Email"
+//         onChange={handleChange}
+//         name="email"
+//         value={formData.email}
+//       />
+//       {/*
+//        * Challenge: Add a textarea for "comments" to the form
+//        * Make sure to update state when it changes.
+//        */}
+//       <textarea
+//         value={formData.comments}
+//         placeholder="Comments"
+//         name="comments"
+//         onChange={handleChange}
+//       />
+//       <input
+//         type="checkbox"
+//         id="isFriendly"
+//         checked={formData.isFriendly}
+//         onChange={handleChange}
+//         name="isFriendly"
+//       />
+//       <label htmlFor="isFriendly">Are you friendly?</label>
+//       <fieldset>
+//         <legend>Current employment status</legend>
+
+//         <input
+//           type="radio"
+//           id="unemployed"
+//           name="employment"
+//           value="unemployed"
+//           checked={formData.employment === "unemployed"}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="unemployed">Unemployed</label>
+//         <br />
+
+//         <input
+//           type="radio"
+//           id="part-time"
+//           name="employment"
+//           value="part-time"
+//           checked={formData.employment === "part-time"}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="part-time">Part-time</label>
+//         <br />
+
+//         <input
+//           type="radio"
+//           id="full-time"
+//           name="employment"
+//           value="full-time"
+//           checked={formData.employment === "full-time"}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="full-time">Full-time</label>
+//         <br />
+//       </fieldset>
+//       <label htmlFor="favColor">What is your favorite color?</label>
+//       <select
+//         id="favColor"
+//         value={formData.favColor}
+//         onChange={handleChange}
+//         name="favColor"
+//       >
+//         <option value="">-- Choose --</option>
+//         <option value="red">Red</option>
+//         <option value="orange">Orange</option>
+//         <option value="yellow">Yellow</option>
+//         <option value="green">Green</option>
+//         <option value="blue">Blue</option>
+//         <option value="indigo">Indigo</option>
+//         <option value="violet">Violet</option>
+//       </select>
+//       <button>Submit</button>
+//     </form>
+//   );
+// }
+
+export default function App() {
   /**
-   * Challenge:
-   * - If there are no unread messages, display "You're all caught up!"
-   * - If there are > 0 unread messages, display "You have <n> unread
-   *   message(s)"
-   *      - If there's exactly 1 unread message, it should read "message"
-   *        (singular)
+   * Challenge: Connect the form to local state
+   *
+   * 1. Create a state object to store the 4 values we need to save.
+   * 2. Create a single handleChange function that can
+   *    manage the state of all the inputs and set it up
+   *    correctly
+   * 3. When the user clicks "Sign up", check if the
+   *    password & confirmation match each other. If
+   *    so, log "Successfully signed up" to the console.
+   *    If not, log "passwords to not match" to the console.
+   * 4. Also when submitting the form, if the person checked
+   *    the "newsletter" checkbox, log "Thanks for signing
+   *    up for our newsletter!" to the console.
    */
-  const displayFrase =
-    messages.length > 0
-      ? `You have ${messages.length} unread message${
-          messages.length > 1 ? "s" : ""
-        }`
-      : "You're all caught up!";
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    newsletter: true,
+  });
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { password, passwordConfirm, newsletter } = formData;
+    if (password === passwordConfirm) console.log("Successfully signed up");
+    else {
+      console.log("Passwords do not match");
+      return;
+    }
+    if (newsletter) console.log("Thanks for signing up for our newsletter!");
+  }
+
   return (
-    <div>
-      <h1>{displayFrase}</h1>
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form--input"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          className="form--input"
+          name="passwordConfirm"
+          value={formData.passwordConfirm}
+          onChange={handleChange}
+        />
+
+        <div className="form--marketing">
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            checked={formData.newsletter}
+            name="newsletter"
+            onChange={handleChange}
+          />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+        </div>
+        <button className="form--submit">Sign up</button>
+      </form>
     </div>
   );
 }
